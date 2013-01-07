c1f3c32132fa01e4c6a0c252a4cf7ebdccc62ad0
nathaniel k smith <nathanielksmith@gmail.com>
2013-01-07 11:26:30 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var jsdom = require('jsdom');
+var _ = require('underscore');
+
+var b = function(tests) {
+    var setUp = tests.setUp || function(cb) { cb() };
+    var tearDown = tests.tearDown || function(cb) { cb() };
+    var html = b._html;
+    var reqs = b._reqs;
+
+    tests.setUp = function(cb) {
+        var testcase = this;
+        jsdom.env(html, reqs, function(err, window) {
+            testcase.window = window;
+            setUp.call(testcase, cb, window);
+        });
+    };
+
+    tests.tearDown = function(cb) {
+        tearDown.call(this, cb, this.window);
+    };
+
+    // set up test methods
+    _(tests).chain()
+        .functions()
+        .filter(function(funcName) {
+            return funcName.match(/^test/i);
+        })
+        .each(function(funcName) {
+            var func = tests[funcName];
+            tests[funcName] = function(test) {
+                return func.call(this, test, this.window);
+            };
+        });
+
+    return tests;
+};
+
+b._root = __dirname;
+b.setRequireRoot = function(root) { b._root = root; };
+
+b._html = '<html><head></head><body></body></html>';
+b.html = function(html) { b._html = html; };
+
+b._reqs = [];
+b.require = function(reqs) {
+    _(reqs).each(function(req) {
+        // if relative, prepend __dirname
+        if (!req.match(new RegExp('^'+path.sep))) {
+            req = path.join(b._root, req);
+        }
+        b._reqs.push(req);
+    });
+};
+
+
+module.exports = b;
---
+++ test/test.js
@@ -0,0 +1,11 @@
+var b = require(__dirname+'../index.js');
+var _ = require('underscore');
+
+exports.test_wrapping ={
+};
+
+exports.test_setters = {
+};
+
+exports.test_requiring = {
+};
