eb81fad0409cd98a5cddfb168acaa732e330bd89
nathaniel k smith <nathanielksmith@gmail.com>
2013-03-28 12:19:11 -0400

    make naming convention consistent

--- test/test.js
+++ test/test.js
@@ -33,82 +33,82 @@ exports.testMeta = {
         test.done();
     },
 
-    test_meta_provide: function(test) {
-        var mock_window = {
+    testMetaProvide: function(test) {
+        var mockWindow = {
             one: 1,
             two: 2,
             three: 3
         };
-        var mock_jsdom = {
+        var mockJsdom = {
             env: function(html, reqs, cb) { cb(); }
         };
-        this.b.__set__({jsdom: mock_jsdom});
-        var test_provides = ['one','two','three'];
-        var test_obj = {
-            provide: test_provides,
-            test_func0: function(t, w, one, two, three) {
+        this.b.__set__({jsdom: mockJsdom});
+        var testProvides = ['one','two','three'];
+        var testObj = {
+            provide: testProvides,
+            testFunc0: function(t, w, one, two, three) {
                 test.equal(one, 1, 'see 1st provided arg');
                 test.equal(two, 2, 'see 2nd provided arg');
                 test.equal(three, 3, 'see 3rd provided arg');
 
             }
         };
-        var computed_test_obj = this.b(test_obj);
-        computed_test_obj.test_func0.call({
-            window: mock_window
+        var computedTestObj = this.b(testObj);
+        computedTestObj.testFunc0.call({
+            window: mockWindow
         }, {});
         test.done()
     },
 
-    test_meta_additional_injects_empty: function(test) {
-        var test_injects = ['one', 'two'];
-        var test_obj = {
-            injects: test_injects
+    testMetaAdditionalInjectsEmpty: function(test) {
+        var testInjects = ['one', 'two'];
+        var testObj = {
+            injects: testInjects
         };
-        var mock_env = m.create_func();
+        var mockEnv = m.create_func();
         this.b.__set__('jsdom', {
-            env: mock_env
+            env: mockEnv
         });
-        var computed_test_obj = this.b(test_obj);
-        computed_test_obj.setUp(m.noop);
-        test.ok(mock_env.called, 'sanity: called env');
-        test.deepEqual(mock_env.args[0][1], test_injects, 'see additional injects');
+        var computedTestObj = this.b(testObj);
+        computedTestObj.setUp(m.noop);
+        test.ok(mockEnv.called, 'sanity: called env');
+        test.deepEqual(mockEnv.args[0][1], testInjects, 'see additional injects');
         test.done();
     },
 
-    test_meta_additional_injects_nonempty: function(test) {
-        var test_injects = ['two', 'three'];
+    testMetaAdditionalInjectsNonempty: function(test) {
+        var testInjects = ['two', 'three'];
         this.b.inject([
             'one',
             'two',
         ]);
-        var test_obj = {
+        var testObj = {
             syntaxCheck: false,
-            injects: test_injects
+            injects: testInjects
         };
-        var mock_env = m.create_func();
+        var mockEnv = m.create_func();
         this.b.__set__('jsdom', {
-            env: mock_env
+            env: mockEnv
         });
-        var computed_test_obj = this.b(test_obj);
-        computed_test_obj.setUp(m.noop);
-        test.ok(mock_env.called, 'sanity: called env');
-        test.deepEqual(mock_env.args[0][1], ['one', 'two', 'three'], 'see additional injects');
+        var computedTestObj = this.b(testObj);
+        computedTestObj.setUp(m.noop);
+        test.ok(mockEnv.called, 'sanity: called env');
+        test.deepEqual(mockEnv.args[0][1], ['one', 'two', 'three'], 'see additional injects');
         test.done();
     },
 
 };
 
-exports.test_wrapping = {
+exports.testWrapping = {
     setUp: function(cb) {
         this.b = rewire(path.join(__dirname, '../index.js'));
         cb();
     },
 
-    test_syntax_check_ok: function(test) {
+    testSyntaxCheckOk: function(test) {
         var b = this.b;
         var tests = {};
-        var mock_fs = {
+        var mockFs = {
             existsSync: m.create_func({return_value: true}),
             readFileSync: m.create_func({
                 return_value: {
@@ -116,20 +116,20 @@ exports.test_wrapping = {
                 }
             })
         };
-        b.__set__('fs', mock_fs);
+        b.__set__('fs', mockFs);
         b.injects = ['bogus', 'yup'];
         test.doesNotThrow(function() {
             b(tests);
         });
-        test.equal(mock_fs.readFileSync.calls, 2, 'read two files');
+        test.equal(mockFs.readFileSync.calls, 2, 'read two files');
 
         test.done();
     },
 
-    test_syntax_check_fail: function(test) {
+    testSyntaxCheckFail: function(test) {
         var b = this.b;
         var tests = {};
-        var mock_fs = {
+        var mockFs = {
             existsSync: m.create_func({return_value: true}),
             readFileSync: m.create_func({
                 return_value: {
@@ -137,99 +137,99 @@ exports.test_wrapping = {
                 }
             })
         };
-        b.__set__('fs', mock_fs);
+        b.__set__('fs', mockFs);
         b.injects = ['bogus', 'yup'];
         test.throws(function() {
             b(tests);
         });
-        test.equal(mock_fs.readFileSync.calls, 1, 'read one file');
+        test.equal(mockFs.readFileSync.calls, 1, 'read one file');
 
         test.done();
     },
 
-    test_just_tests: function(test) {
-        var mock_test = m.create_mock();
-        var mock_dom = m.create_mock();
-        var mock_env = m.create_func({func:function(html, injects, cb) {
-            cb(null, mock_dom);
+    testJustTests: function(test) {
+        var mockTest = m.create_mock();
+        var mockDom = m.create_mock();
+        var mockEnv = m.create_func({func:function(html, injects, cb) {
+            cb(null, mockDom);
         }});
-        this.b.__set__('jsdom', { env: mock_env, });
-        var test_func_0 = m.create_func();
-        var test_func_1 = m.create_func();
-        var test_obj = {
-            test_func_0: test_func_0,
-            test_func_1: test_func_1
+        this.b.__set__('jsdom', { env: mockEnv, });
+        var testFunc0 = m.create_func();
+        var testFunc1 = m.create_func();
+        var testObj = {
+            testFunc0: testFunc0,
+            testFunc1: testFunc1
         };
 
-        var computed_test_obj = this.b(test_obj);
+        var computedTestObj = this.b(testObj);
 
-        test.ok(_(computed_test_obj).has('setUp'), 'setUp added');
-        test.ok(_(computed_test_obj).has('tearDown'), 'tearDown added');
+        test.ok(_(computedTestObj).has('setUp'), 'setUp added');
+        test.ok(_(computedTestObj).has('tearDown'), 'tearDown added');
 
-        var tearDown = computed_test_obj.tearDown;
-        computed_test_obj.setUp(function() {
-            test.equal(computed_test_obj.window, mock_dom, 'see mock_dom');
-            computed_test_obj.test_func_0(mock_test);
-            test.equal(test_func_0.calls, 1, 'see 0th func called');
-            test.deepEqual(test_func_0.args[0], [mock_test, mock_dom], 'see proper args');
-            computed_test_obj.test_func_1(mock_test);
-            test.equal(test_func_1.calls, 1, 'see 0th func called');
-            test.deepEqual(test_func_1.args[0], [mock_test, mock_dom], 'see proper args');
+        var tearDown = computedTestObj.tearDown;
+        computedTestObj.setUp(function() {
+            test.equal(computedTestObj.window, mockDom, 'see mockDom');
+            computedTestObj.testFunc0(mockTest);
+            test.equal(testFunc0.calls, 1, 'see 0th func called');
+            test.deepEqual(testFunc0.args[0], [mockTest, mockDom], 'see proper args');
+            computedTestObj.testFunc1(mockTest);
+            test.equal(testFunc1.calls, 1, 'see 0th func called');
+            test.deepEqual(testFunc1.args[0], [mockTest, mockDom], 'see proper args');
             test.done();
         });
     },
 
-    test_with_setUp_tearDown: function(test) {
-        var mock_test = m.create_mock();
-        var mock_dom = m.create_mock();
-        var mock_env = m.create_func({func:function(html, injects, cb) {
-            cb(null, mock_dom);
+    testWithSetUpTearDown: function(test) {
+        var mockTest = m.create_mock();
+        var mockDom = m.create_mock();
+        var mockEnv = m.create_func({func:function(html, injects, cb) {
+            cb(null, mockDom);
         }});
-        this.b.__set__('jsdom', { env: mock_env, });
-        var test_func_0 = m.create_func();
-        var test_func_1 = m.create_func();
-        var test_setup = m.create_func({func:function(cb){cb();}});
-        var test_teardown = m.create_func({func:function(cb){cb();}});
-        var test_obj = {
-            setUp: test_setup,
-            tearDown: test_teardown,
-            test_func_0: test_func_0,
-            test_func_1: test_func_1
+        this.b.__set__('jsdom', { env: mockEnv, });
+        var testFunc0 = m.create_func();
+        var testFunc1 = m.create_func();
+        var testSetup = m.create_func({func:function(cb){cb();}});
+        var testTeardown = m.create_func({func:function(cb){cb();}});
+        var testObj = {
+            setUp: testSetup,
+            tearDown: testTeardown,
+            testFunc0: testFunc0,
+            testFunc1: testFunc1
         };
 
-        var computed_test_obj = this.b(test_obj);
+        var computedTestObj = this.b(testObj);
 
-        test.ok(_(computed_test_obj).has('setUp'), 'setUp retained');
-        test.ok(_(computed_test_obj).has('tearDown'), 'tearDown retained');
+        test.ok(_(computedTestObj).has('setUp'), 'setUp retained');
+        test.ok(_(computedTestObj).has('tearDown'), 'tearDown retained');
 
-        computed_test_obj.setUp(function() {
-            test.equal(test_setup.calls, 1, 'see call to test setup');
-            test.equal(computed_test_obj.window, mock_dom, 'see mock_dom');
-            test.equal(test_setup.args[0][1], mock_dom, 'see mock_dom in setup');
+        computedTestObj.setUp(function() {
+            test.equal(testSetup.calls, 1, 'see call to test setup');
+            test.equal(computedTestObj.window, mockDom, 'see mockDom');
+            test.equal(testSetup.args[0][1], mockDom, 'see mockDom in setup');
 
-            computed_test_obj.test_func_0(mock_test);
-            test.equal(test_func_0.calls, 1, 'see 0th func called');
-            test.deepEqual(test_func_0.args[0], [mock_test, mock_dom], 'see proper args');
+            computedTestObj.testFunc0(mockTest);
+            test.equal(testFunc0.calls, 1, 'see 0th func called');
+            test.deepEqual(testFunc0.args[0], [mockTest, mockDom], 'see proper args');
 
-            computed_test_obj.test_func_1(mock_test);
-            test.equal(test_func_1.calls, 1, 'see 1st func called');
-            test.deepEqual(test_func_1.args[0], [mock_test, mock_dom], 'see proper args');
+            computedTestObj.testFunc1(mockTest);
+            test.equal(testFunc1.calls, 1, 'see 1st func called');
+            test.deepEqual(testFunc1.args[0], [mockTest, mockDom], 'see proper args');
 
-            computed_test_obj.tearDown(function(){});
-            test.equal(test_teardown.calls, 1, 'see call to test teardown');
-            test.equal(test_teardown.args[0][1], mock_dom, 'see mock_dom in teardown');
+            computedTestObj.tearDown(function(){});
+            test.equal(testTeardown.calls, 1, 'see call to test teardown');
+            test.equal(testTeardown.args[0][1], mockDom, 'see mockDom in teardown');
 
             test.done();
         });
     }
 };
 
-exports.test_setters = {
+exports.testSetters = {
     setUp: function(cb) {
         this.b = require(path.join(__dirname, '../index.js'));
         cb();
     },
-    test_set_root: function(test) {
+    testSetRoot: function(test) {
         var root = '../../';
         this.b.setInjectRoot(root);
         test.equal(this.b.root, root, 'set root');
@@ -237,7 +237,7 @@ exports.test_setters = {
     }
 };
 
-exports.test_injecting = {
+exports.testInjecting = {
     setUp: function(cb) {
         this.b = require(path.join(__dirname, '../index.js'));
         cb();
@@ -246,30 +246,30 @@ exports.test_injecting = {
         this.b.injects = [];
         cb();
     },
-    test_inject_one: function(test) {
+    testInjectOne: function(test) {
         var inject = '/injection';
         this.b.inject(inject);
         test.deepEqual(this.b.injects, [inject], 'see single inject in injects');
         test.done()
     },
-    test_additive: function(test) {
-        var inject_one = '/inject0';
-        var inject_two = ['/inject1', '/inject2'];
-        this.b.inject(inject_one);
-        this.b.inject(inject_two);
-        var computed_injects = this.b.injects;
-        test.equal(_(computed_injects).difference(inject_two.concat([inject_one])), 0, 'got all injects, unchanged');
+    testAdditive: function(test) {
+        var injectOne = '/inject0';
+        var injectTwo = ['/inject1', '/inject2'];
+        this.b.inject(injectOne);
+        this.b.inject(injectTwo);
+        var computedInjects = this.b.injects;
+        test.equal(_(computedInjects).difference(injectTwo.concat([injectOne])), 0, 'got all injects, unchanged');
         test.done();
     },
-    test_inject_absolute: function(test) {
+    testInjectAbsolute: function(test) {
         var injects = [
             '/hi/there',
             '/what/is/the/haps',
         ];
         this.b.inject(injects);
-        var computed_injects = this.b.injects;
-        test.equal(computed_injects.length, 2, 'see 2 injects');
-        test.equal(_(computed_injects).difference(injects).length, 0, 'got all injects, unchanged');
+        var computedInjects = this.b.injects;
+        test.equal(computedInjects.length, 2, 'see 2 injects');
+        test.equal(_(computedInjects).difference(injects).length, 0, 'got all injects, unchanged');
         test.done();
     },
     test_inject_relative: function(test) {
@@ -280,8 +280,8 @@ exports.test_injecting = {
         var root = '../';
         this.b.setInjectRoot(root);
         this.b.inject(injects);
-        var computed_injects = this.b.injects;
-        test.equal(_(computed_injects).difference(
+        var computedInjects = this.b.injects;
+        test.equal(_(computedInjects).difference(
             _(injects).map(function(s) { return root + s })
         ).length, 0, 'see computed paths');
         test.done();
